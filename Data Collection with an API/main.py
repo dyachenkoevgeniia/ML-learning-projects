fcb1820047c6e2476e4344b87aac79c1576169e2
dyachenkoevgeniia <dyachenkoevgenia1@gmail.com>
2025-10-11 20:46:28 +0300

    first commit

---
+++ Data Collection with an API/main.py
@@ -0,0 +1,35 @@
+import requests
+import base64
+from func import get_trending_playlist_data
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+
+client_credentials = f"{CLIENT_ID}:{CLIENT_SECRET}"
+client_credentials_base64 = base64.b64encode(client_credentials.encode())
+
+token_url = 'https://accounts.spotify.com/api/token'
+headers = {
+    'Authorization': f'Basic {client_credentials_base64.decode()}'
+}
+data = {
+    'grant_type': 'client_credentials'
+}
+response = requests.post(token_url, data=data, headers=headers)
+
+if response.status_code == 200:
+    access_token = response.json()['access_token']
+    print("Access token obtained successfully.")
+else:
+    print("Error obtaining access token.")
+    exit()
+
+playlist_id = '1gfWsOG1WAoxNeUMMktZbq'
+music_df = get_trending_playlist_data(playlist_id, access_token)
+
+print(music_df)
+music_df.to_csv("musicdata.csv")
