175c2fcceade8bec4683a0cdd48a8c6b79a1881a
dyachenkoevgeniia <dyachenkoevgenia1@gmail.com>
2026-02-21 20:05:26 +0200

    Delete file

--- Data Collection with an API/collector.py
+++ Data Collection with an API/collector.py
@@ -4,31 +4,45 @@ import spotipy
 def fetch_playlist_tracks(playlist_id, access_token):
     sp = spotipy.Spotify(auth=access_token)
 
-    playlist_tracks = sp.playlist_tracks(
-        playlist_id,
-        fields='items(track(id, name, artists, album(id, name), popularity, explicit, external_urls))'
-    )
+    playlist_tracks = sp.playlist_tracks(playlist_id, fields='items(track(id, name, artists, album(id, name)))')
 
     music_data = []
-
-    for item in playlist_tracks['items']:
-        track = item['track']
-
+    for track_info in playlist_tracks['items']:
+        track = track_info['track']
         track_name = track['name']
-        artists = ', '.join(artist['name'] for artist in track['artists'])
+        artists = ', '.join([artist['name'] for artist in track['artists']])
         album_name = track['album']['name']
+        album_id = track['album']['id']
         track_id = track['id']
 
+        audio_features = None
+
+        try:
+            album_info = sp.album(album_id) if album_id != 'Not available' else None
+            release_date = album_info['release_date'] if album_info else None
+        except:
+            release_date = None
+
+        try:
+            track_info = sp.track(track_id) if track_id != 'Not available' else None
+            popularity = track_info['popularity'] if track_info else None
+        except:
+            popularity = None
+
         track_data = {
-            'track_name': track_name,
-            'artists': artists,
-            'album_name': album_name,
-            'track_id': track_id,
-            'popularity': track.get('popularity'),
-            'explicit': track.get('explicit'),
-            'spotify_url': track.get('external_urls', {}).get('spotify')
+            'Track Name': track_name,
+            'Artists': artists,
+            'Album Name': album_name,
+            'Album ID': album_id,
+            'Track ID': track_id,
+            'Popularity': popularity,
+            'Release Date': release_date,
+            'Explicit': track_info.get('explicit', None),
+            'External URLs': track_info.get('external_urls', {}).get('spotify', None),
         }
 
         music_data.append(track_data)
 
-    return pd.DataFrame(music_data)
+    df = pd.DataFrame(music_data)
+
+    return df
